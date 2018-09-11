f232a59aee648e9436de3244f28eb3f6ccfb9d09
Krisztian Horvath <keyki.kk@gmail.com>
2018-09-11 16:47:00 +0200

    simple cli use case

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 build-krisz:
 	GOOS=darwin CGO_ENABLED=1 go build -buildmode=plugin
+
+build-dlm:
+	GOOS=darwin CGO_ENABLED=0 go build -o dlm
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/urfave/cli"
+	"os"
 )
 
 func Commander() []cli.Command {
@@ -12,8 +13,27 @@ func Commander() []cli.Command {
 			Description: "testcommand",
 			Usage:       "invoke the command and see what happens",
 			Action:      MyCommand,
+			Flags:       []cli.Flag{FlName},
+			BashComplete: func(c *cli.Context) {
+				printFlagCompletion(FlName)
+			},
 		},
 	}
+}
+
+type StringFlag struct {
+	cli.StringFlag
+}
+
+var FlName = StringFlag{
+	StringFlag: cli.StringFlag{
+		Name:  "name",
+		Usage: "name of resource",
+	},
+}
+
+func printFlagCompletion(f cli.Flag) {
+	fmt.Printf("--%s\n", f.GetName())
 }
 
 type greeting string
@@ -25,4 +45,14 @@ func (g greeting) Greet() {
 var Greeter greeting
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "dlm"
+	app.HelpName = "Hortonworks Data Lifecycle command line tool"
+	app.Author = "Hortonworks"
+	app.EnableBashCompletion = true
+	app.Commands = Commander()
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
 }
